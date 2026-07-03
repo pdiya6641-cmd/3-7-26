b16b9136892309031ad4314f29d29b235427ecfc
pdiya6641-cmd <pdiya6641@gmail.com>
2026-07-03 09:17:17 +0530

    Update h3.py

--- h3.py
+++ h3.py
@@ -1,6 +1,25 @@
-list1 = [1, 3, 5]
-list2 = [2, 4, 6]
+mobile = [2, 5, 8, 12]
+counter = [1, 3, 7, 10, 15]
 
-result = sorted(list1 + list2)
+merged = []
 
-print(result)
+i = 0
+j = 0
+
+while i < len(mobile) and j < len(counter):
+    if mobile[i] < counter[j]:
+        merged.append(mobile[i])
+        i += 1
+    else:
+        merged.append(counter[j])
+        j += 1
+
+while i < len(mobile):
+    merged.append(mobile[i])
+    i += 1
+
+while j < len(counter):
+    merged.append(counter[j])
+    j += 1
+
+print("Final Waitlist:", merged)
