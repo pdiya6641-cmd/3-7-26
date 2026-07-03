9f64627ed11122b86c8366f83b5a87a5f909b389
pdiya6641-cmd <pdiya6641@gmail.com>
2026-07-03 12:42:15 +0530

    Update h1.py

--- h1.py
+++ h1.py
@@ -2,7 +2,7 @@ l1 = ["diya@gmail.com","jiya@gmail.com"]
 s = input("Enter Your ID: ")
 
 if s in l1:
-    print("Spam Sender")
+    print("Spam")
 else:
-    print("Safe Sender")
+    print("Safe")
 	
